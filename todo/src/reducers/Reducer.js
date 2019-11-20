7f65504ffee8d8ed7aa8967e3e7a2d791b4f7378
Jay Leach <42565053+leachcoding@users.noreply.github.com>
2019-11-19 20:56:38 -0500

    Step 1 completed

---
+++ todo/src/reducers/Reducer.js
@@ -0,0 +1,13 @@
+// Initial State Object
+
+export const toDos = [{
+  item: 'Learn about reducers',
+  completed: false,
+  id: 3892987589
+}];
+
+export const reducer = (state, action) => {
+  switch (action.type) {
+    default: return state;
+  }
+};
